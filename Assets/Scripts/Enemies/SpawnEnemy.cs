aa9624b2517e420541843e801702e64b977d52a0
Le Quang Hieu <105836140+hieuday1572@users.noreply.github.com>
2024-03-19 13:19:52 +0700

    update-spawn

--- Assets/Scripts/Enemies/SpawnEnemy.cs
+++ Assets/Scripts/Enemies/SpawnEnemy.cs
@@ -7,6 +7,8 @@ public class SpawnEnemy : MonoBehaviour
     // needed for spawning
     [SerializeField]
     GameObject prefabEnemies;
+    [SerializeField]
+    GameObject prefabEnemies2;
 
     // spawn control
     const float MinSpawnDelay = 1;
@@ -14,10 +16,6 @@ public class SpawnEnemy : MonoBehaviour
     Timer spawnTimer;
 
     // spawn location support
-    const int SpawnBorderSize = 100;
-    float minSpawnX;
-    float maxSpawnX;
-    float SpawnY;
     int limit;
 
 
@@ -27,9 +25,6 @@ public class SpawnEnemy : MonoBehaviour
     void Start()
     {
         // save spawn boundaries for efficiency
-        minSpawnX = 47.02f;
-        maxSpawnX = 65.86f;
-        SpawnY = -8.84f;
         limit = 1;
         // create and start timer
         spawnTimer = gameObject.AddComponent<Timer>();
@@ -43,9 +38,9 @@ public class SpawnEnemy : MonoBehaviour
     void Update()
     {
         // check for time to spawn a new teddy bear
-        if (spawnTimer.Finished && limit<=4)
+        if (spawnTimer.Finished && limit<=8)
         {
-            SpawnEnemiy();
+            SpawnEnemies();
             limit++;
             // change spawn timer duration and restart
             spawnTimer.Duration = Random.Range(MinSpawnDelay, MaxSpawnDelay);
@@ -56,13 +51,46 @@ public class SpawnEnemy : MonoBehaviour
     /// <summary>
     /// Spawns a new teddy bear at a random location
     /// </summary>
-    void SpawnEnemiy()
+    void SpawnEnemies()
     {
         // generate random location and create new teddy bear
-        Vector3 location = new Vector3(Random.Range(minSpawnX, maxSpawnX),
-            SpawnY,
-            -0.03995441f);
-        GameObject enemies = Instantiate(prefabEnemies) as GameObject;
-        enemies.transform.position = location;
+        Vector3 location;
+        Vector3 location2;
+        int spriteNumber = Random.Range(0, 2);
+        if(spriteNumber == 0)
+        {
+            GameObject enemies = Instantiate(prefabEnemies) as GameObject;
+            location = new Vector3(Random.Range(54.02f, 65.86f),
+            -8.84f, -0.03995441f);
+            location2 = new Vector3(Random.Range(35.43f, 38.69f),
+            -3.84f, -0.03995441f); ;
+            int locationNumber = Random.Range(0, 2);
+            if(locationNumber == 0)
+            {
+                enemies.transform.position = location;
+            }
+            else
+            {
+                enemies.transform.position = location2;
+            }
+
+        }
+        else
+        {
+            GameObject enemies = Instantiate(prefabEnemies2) as GameObject;
+            location = new Vector3(Random.Range(55.57f, 59.18f),
+            -7.88f, -4.113077f);
+            location2 = new Vector3(Random.Range(35f, 38.68f),
+            -2.66f, -4.113077f); ;
+            int locationNumber = Random.Range(0, 2);
+            if (locationNumber == 0)
+            {
+                enemies.transform.position = location;
+            }
+            else
+            {
+                enemies.transform.position = location2;
+            }
+        }
     }
 }
